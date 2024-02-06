c50029563bf84c39dd22b0a66996538c84519086
CodeBustler <codebustler360online@gmail.com>
2024-02-06 15:34:41 +0530

    ADD TapToTop

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SideBar() {
+	return (
+		<div className="h-[100vh] bg-gray-800 backdrop-blur-3xl">SideBar</div>
+	);
+}
+
+export default SideBar;
---
+++ src/components/TapToTop.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa6";
+
+function TapToTop() {
+	const [scrollTop, setScrollTop] = useState(0);
+
+	// --------------------------------------------------
+	// ***************** TAP TO TOP *****************
+	// --------------------------------------------------
+	const handleScroll = () => {
+		setScrollTop(window.scrollY);
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		//WHEN COMPONENT UNMOUNTS
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+	// --------------------------------------------------
+
+	return (
+		<div>
+			{scrollTop > 800 && (
+				<FaArrowUp
+					className=" w-10 h-10 bg-yellow-500 p-3 rounded-full shadow-xl fixed bottom-10 right-10 z-50 cursor-pointer text-gray-900 opacity-30 hover:opacity-100 hover:scale-125 transition"
+					onClick={() => window.scrollTo(0, 0)}
+				/>
+			)}
+		</div>
+	);
+}
+
+export default TapToTop;
