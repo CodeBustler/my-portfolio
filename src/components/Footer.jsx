084a6d3faa6534be1f2bc4ff87e3facea493bf3c
CodeBustler <codebustler360online@gmail.com>
2025-07-09 10:41:03 +0530

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
 	return (
 		<footer className="bg-[#1D2430] text-gray-500  text-center py-5 sticky w-[100%] mt-24">
 			<a href="https://www.instagram.com/codebustler" target="blank">
-				By Mallikarjun | CodeBustler
+				@2025 By Mallikarjun | CodeBustler
 			</a>
 		</footer>
 	);
